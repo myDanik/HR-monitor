fab9d2a7e4782487a066ef257fa0c840dba85a40
DaniilBPI2301 <den.spesivtsev.006@gmail.com>
2025-04-24 00:06:56 +0300

    user, resume, vacancy, stats repositories has been added

--- pkg/repository/stats_repo.go
+++ pkg/repository/stats_repo.go
@@ -2,11 +2,10 @@ package repository
 
 import (
 	"context"
-	"time"
 )
 
 type StatsRepository interface {
-	GetAverageStageTime(ctx context.Context, stageID int) (time.Duration, error)
+	GetAverageStageTime(ctx context.Context, stageID int) (float64, error)
 	GetStageDistribution(ctx context.Context) (map[string]int, error)
 	GetSourceDistribution(ctx context.Context) (map[string]int, error)
 	GetAverageCandidatesPerVacancy(ctx context.Context) (float64, error)
